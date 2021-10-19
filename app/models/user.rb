afca7acf61e714a9eaf1d1e7987d1b02674c7412
vagrant <rpg7ofdeath@gmail.com>
2021-10-19 20:16:16 +0000

    make user-spec pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
+  validates :password, presence: true, length: { minimum: 4 }
+  validates :password_confirmation, presence: true
+
   def self.authenticate_with_credentials(email, password)
     @user = User.find_by_email(email.strip.downcase)
     if @user && @user.authenticate(password)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,75 +4,64 @@ RSpec.describe User, type: :model do
   
   describe 'Validations' do
     before do 
-      @user = User.new(:email => "foo@bar.com", :name => "foo bar", :password => "foobar", :password_confirmation => "foobar")
+      @test_user = User.new(:email => "foo@bar.com", :name => "foo bar", :password => "foobar", :password_confirmation => "foobar")
     end
     it "passes when all fields are set" do
-      @user.valid?
-      expect(@user.errors.full_messages).to be_empty
+      expect(@test_user.valid?).to eq(true)
     end
 
     it "fails when name is nil" do
-      @user.name = nil
-      @user.valid?
-      expect(@user.errors.full_messages).to eq([])
+      @test_user.name = nil
+      expect(@test_user.valid?).to eq(false)
     end
 
     it "fails when password is nil" do
-      @user.password = nil
-      @user.valid?
-      expect(@user.errors.full_messages).to include "Password can't be blank"
+      @test_user.password = nil
+      @test_user.valid?
+      expect(@test_user.errors.full_messages).to include "Password can't be blank"
     end
 
     it "fails when password_confirmation is nil" do
-      @user.password_confirmation = nil
-      @user.valid?
-      
-      expect(@user.errors.full_messages).to eq([])
+      @test_user.password_confirmation = nil
+      expect(@test_user.valid?).to eq(false)
     end
 
     it "fails when password and password_confirmation does not match" do
-      @user.password_confirmation = "barfoo"
-      @user.valid?
-      expect(@user.errors.full_messages).not_to be_empty
+      @test_user.password_confirmation = "barfoo"
+      expect(@test_user.valid?).to eq(false)
     end
 
     it "fails when email address is not unique" do
       User.create(:email => "foo@bar.com", :name => "foo bar", :password => "foobar", :password_confirmation => "foobar")
-      @user.valid?
-      expect(@user.errors.full_messages).to eq([])
+      expect(@test_user.valid?).to eq(false)
     end
 
     it "fails when password length is less than 4" do
-      @user.password = "123"
-      @user.password_confirmation = "123"
-      @user.valid?
-      expect(@user.errors.full_messages).to eq([])
+      @test_user.password = "123"
+      @test_user.password_confirmation = "123"
+      expect(@test_user.valid?).to eq(false)
     end
   end
 
   describe '.authenticate_with_credentials' do
     it "returns user when successfully authenticate" do
-      @user.save
       user = User.authenticate_with_credentials("foo@bar.com", "foobar")
-      expect(user).to be == @user
+      expect(user).to be == @test_user
     end
 
     it "returns nil when authentication fails" do
-      @user.save
       user = User.authenticate_with_credentials("foo@bar.com", "barfoo")
       expect(user).to be == nil
     end
 
     it "passes user auth" do
-      @user.save
       user = User.authenticate_with_credentials("  foo@bar.com  ", "foobar")
-      expect(user).to be == @user
+      expect(user).to be == @test_user
     end
 
     it "successfully auths wrong lettercase" do
-      @user.save
       user = User.authenticate_with_credentials("FOO@BAR.COM", "foobar")
-      expect(user).to be == @user
+      expect(user).to be == @test_user
     end
   end
 
