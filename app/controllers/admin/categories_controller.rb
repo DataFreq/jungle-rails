9d5c0ad29c78a585a6b58b04414012705e7c6cb0
vagrant <rpg7ofdeath@gmail.com>
2021-10-20 21:41:50 +0000

    fix categories controller bug

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -2,7 +2,7 @@ class Admin::CategoriesController < ApplicationController
   http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
 
   def index
-    @categories = Category.order(id: :desc).all
+    @categories = Category.all
   end
 
   def new
@@ -12,7 +12,7 @@ class Admin::CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
 
-    if @product.save
+    if @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
@@ -22,8 +22,6 @@ class Admin::CategoriesController < ApplicationController
   private
 
   def category_params
-    params.require(:category).permit(
-      :name
-    )
+    params.require(:category).permit(:name)
   end
 end
